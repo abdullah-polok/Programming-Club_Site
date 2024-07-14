f93695ffd5abffe1926868d794250447e820c4b8
unknown <abdullah-polok>
2024-07-14 08:44:12 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Components/Headers/Navbar";
+import Header from "./Components/Headers/Navbar";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Navbar></Navbar>
+      </div>
+    </>
+  );
+}
+
+export default App;
